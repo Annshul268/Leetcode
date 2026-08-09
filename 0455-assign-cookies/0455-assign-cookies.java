ed3a153a445ed5ae6b97624a7c259bd63197a410
Anshul Kumar Gupta <officialanshul378@gmail.com>
2026-08-10 01:59:44 +0530

    Time: 15 ms (59.8%), Space: 51.4 MB (56.21%) - LeetHub

---
+++ 0455-assign-cookies/0455-assign-cookies.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int findContentChildren(int[] g, int[] s) {
+        int n = g.length;
+        int m = s.length;
+
+        Arrays.sort(g);
+        Arrays.sort(s);
+
+        int i = 0;
+        int j = 0;
+
+        int count = 0;
+        while(i < n && j < m){
+            // if able to fulfil greed then count and move both pointer
+            if(s[j] >= g[i]){
+                count++;
+                i++;
+                j++;
+            }else{
+                j++; //(move cookies pointer maybe next cookie can satisfy the current child)
+            }
+        }
+        return count;
+    }
+}
